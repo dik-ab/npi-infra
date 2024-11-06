3e77d33a08f5cfee6058ea2dfc6ef7c253e21e6c
dik-ab <abedaiki06@gmail.com>
2024-11-06 10:34:43 +0900

    🧱 network(vpc, igw, routing)実装

---
+++ modules/network/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment name, such as dev or prod"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnets_cidrs" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
