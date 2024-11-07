23f1496114c7f4869773d4a04401a5d2734cc977
dik-ab <abedaiki06@gmail.com>
2024-11-07 09:08:18 +0900

    🧱 aurora sg

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -46,3 +46,26 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
+resource "aws_security_group" "aurora_sg" {
+  name        = "${var.project_name}-${var.environment}-aurora-sg"
+  description = "Aurora PostgreSQL security group for private access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 5432  
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-aurora-sg"
+  }
+}
--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -5,3 +5,7 @@ output "alb_sg_id" {
 output "ecs_sg_id" {
   value = aws_security_group.ecs_sg.id
 }
+
+output "aurora_sg_id" {
+  value       = aws_security_group.aurora_sg.id
+}
