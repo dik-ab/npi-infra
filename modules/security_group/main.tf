62a9b0e1b649afcd52741f25a8e0fd3faf9fdeb4
dik-ab <abedaiki06@gmail.com>
2024-11-08 15:18:28 +0900

    🧱 sesへの権限を追加

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   egress {
+    from_port   = 587
+    to_port     = 587
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
