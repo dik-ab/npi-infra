d936a8fc502095eefc3d7d89e4f404614c9db558
dik-ab <abedaiki06@gmail.com>
2024-11-10 17:45:59 +0900

    🧱 codepipeline用のiam定義

---
+++ modules/iam/codepipeline_role/main.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "codepipeline_role" {
+  name = "${var.project_name}-${var.environment}-codepipeline-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "${var.project_name}-${var.environment}-codepipeline-policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*",
+        "codebuild:*",
+        "codedeploy:*",
+        "cloudwatch:*",
+        "iam:PassRole"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
