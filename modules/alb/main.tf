351364159d8e1e76e12bc94d5b3a227d0a2cbeac
dik-ab <abedaiki06@gmail.com>
2024-11-11 09:49:13 +0900

    🧱 blue greenデプロイに伴う修正alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -10,15 +10,16 @@ resource "aws_lb" "ecs_lb" {
   }
 }
 
-resource "aws_lb_target_group" "ecs_tg" {
-  name     = "${var.project_name}-${var.environment}-target-group"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type  = "ip" 
+
+resource "aws_lb_target_group" "ecs_tg_blue" {
+  name        = "${var.project_name}-${var.environment}-target-group-blue"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
 
   health_check {
-    path                = "/health" 
+    path                = "/health"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
@@ -27,7 +28,28 @@ resource "aws_lb_target_group" "ecs_tg" {
   }
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-target-group"
+    Name = "${var.project_name}-${var.environment}-target-group-blue"
+  }
+}
+
+resource "aws_lb_target_group" "ecs_tg_green" {
+  name        = "${var.project_name}-${var.environment}-target-group-green"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-target-group-green"
   }
 }
 
@@ -38,6 +60,69 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    target_group_arn = aws_lb_target_group.ecs_tg_blue.arn
+  }
+  tags = {
+    Name = "${var.project_name}-${var.environment}-alb-listener"
+  }
+}
+
+resource "aws_lb_listener" "http_test" {
+  load_balancer_arn = aws_lb.ecs_lb.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは「HTTP-test」です"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "prod" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_blue.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      action["target_group_arn"],
+    ]
+  }
+}
+
+resource "aws_lb_listener_rule" "test" {
+  listener_arn = aws_lb_listener.http_test.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg_green.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      action["target_group_arn"],
+    ]
   }
 }
--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,14 +1,29 @@
-output "target_group_arn" {
-  value       = aws_lb_target_group.ecs_tg.arn
-  description = "ARN of the target group"
-}
-
-output "target_group_name" {
-  value       = aws_lb_target_group.ecs_tg.name
-  description = "Name of the target group"
-}
-
 output "listener_arn" {
   value       = aws_lb_listener.http.arn
   description = "ARN of the ALB listener"
+}
+
+output "test_listener_arn" {
+  value       = aws_lb_listener.http_test.arn
+  description = "ARN of the ALB test listener"
+}
+
+output "blue_target_group_arn" {
+  value = aws_lb_target_group.ecs_tg_blue.arn
+  description = "ARN of the blue target group for Blue/Green deployments"
+}
+
+output "green_target_group_arn" {
+  value = aws_lb_target_group.ecs_tg_green.arn
+  description = "ARN of the green target group for Blue/Green deployments"
+}
+
+output "lb_blue_target_group_name" {
+  description = "The name of the blue target group"
+  value       = aws_lb_target_group.ecs_tg_blue.name
+}
+
+output "lb_green_target_group_name" {
+  description = "The name of the green target group"
+  value       = aws_lb_target_group.ecs_tg_green.name
 }
