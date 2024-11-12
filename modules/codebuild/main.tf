3266cb7ba40ed4a8e78f1b488de6cce1e6aa4c88
dik-ab <abedaiki06@gmail.com>
2024-11-12 17:46:00 +0900

    🧱 python3.10を使用できるcodebuildのバージョンへと変更

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -8,7 +8,7 @@ resource "aws_codebuild_project" "backend_build" {
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:5.0"
+    image           = "aws/codebuild/standard:7.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
     environment_variable {
