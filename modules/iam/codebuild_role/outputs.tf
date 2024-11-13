1e4d9632d0889aec69de207e0167733beeb5a6a8
dik-ab <abedaiki06@gmail.com>
2024-11-13 10:51:03 +0900

    🧱 マイグレーション用codebuildのrole実装

--- modules/iam/codebuild_role/outputs.tf
+++ modules/iam/codebuild_role/outputs.tf
@@ -2,3 +2,8 @@ output "codebuild_service_role_arn" {
   description = "The ARN of the CodeBuild service role"
   value       = aws_iam_role.codebuild_service_role.arn
 }
+
+output "codebuild_db_role_arn" {
+  description = "The ARN of the CodeBuild role for db migration"
+  value       = aws_iam_role.codebuild_db_role.arn
+}
